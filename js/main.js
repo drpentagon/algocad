8315087b6bf83e21b0150655b8553c580dac0178
Martin Svärd <martinsvard@me.com>
2018-12-07 16:55:37 +0100

    Project setup

---
+++ js/main.js
@@ -0,0 +1,12 @@
+class Application {
+  constructor () {
+    console.log('initialized!')
+  }
+
+  start () {
+    console.log('application start')
+  }
+}
+
+const algocad = new Application()
+algocad.start()
